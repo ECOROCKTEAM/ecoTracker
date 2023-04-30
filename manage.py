707750605db4f7e3662963ed8d07327152a425df
JaysesS <myfantasyisnotworking@vk.com>
2023-04-30 18:31:54 +0300

    Rm comment on manage cli

--- manage.py
+++ manage.py
@@ -5,8 +5,6 @@ from src.cli.cmd.translate.cmd import translate
 @click.group()
 def entry_point():
     pass
-    # click.secho('Hello there', fg="blue", bold=True)
-    # print(TranslationEnum.__subclasses__())
 
 entry_point.add_command(translate)
 

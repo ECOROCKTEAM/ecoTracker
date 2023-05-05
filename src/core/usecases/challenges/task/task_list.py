424df599308e40a44a553da4b20933c9e4a97faf
JaysesS <myfantasyisnotworking@vk.com>
2023-05-06 01:38:22 +0300

    Fix UC Task

--- src/core/usecases/challenges/task/task_list.py
+++ src/core/usecases/challenges/task/task_list.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass, field
 
-from src.core.interfaces.repository.challenges.task import ITaskRepository
+from src.core.interfaces.repository.challenges.task import IRepositoryTask
 from src.core.exception.user import UserIsNotActivateError
 from src.core.entity.task import Task
 from src.core.entity.user import User
+from src.core.dto.mock import MockObj
 
 
 @dataclass
@@ -12,24 +13,25 @@ class Result:
 
 
 class TaskListUseCase:
-    def __init__(self, repo: ITaskRepository) -> None:
+    def __init__(self, repo: IRepositoryTask) -> None:
         self.repo = repo
 
     async def __call__(
         self,
         *,
         user: User,
-        sorting_obj: str = None,
-        paggination_obj: str = None,
-        filter_obj: str = None,
+        sorting_obj: MockObj,
+        paggination_obj: MockObj,
+        filter_obj: MockObj,
     ) -> Result:
         if not user.active:
             raise UserIsNotActivateError(username=user.username)
 
-        task_list = await self.repo.list(
+        task_list = await self.repo.lst(
             sorting_obj=sorting_obj,
             paggination_obj=paggination_obj,
             filter_obj=filter_obj,
+            return_language=user.language,
         )
 
         return Result(items=task_list)

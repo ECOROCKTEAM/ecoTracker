439d89f333678b8f8d7b4b6f54db58c68cbf5142
JaysesS <myfantasyisnotworking@vk.com>
2023-06-03 00:10:06 +0300

    Update mission uc's with score adding

--- src/core/usecases/challenges/mission/mission_community_update.py
+++ src/core/usecases/challenges/mission/mission_community_update.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 
 from src.core.dto.challenges.mission import MissionCommunityUpdateDTO
+from src.core.dto.community.score import CommunityOperationWithScoreDTO
 from src.core.entity.mission import MissionCommunity
 from src.core.entity.user import User
 from src.core.enum.challenges.status import OccupancyStatusEnum
 from src.core.enum.community.role import CommunityRoleEnum
-from src.core.exception.base import EntityNotActive
+from src.core.enum.score.operation import ScoreOperationEnum
+from src.core.exception.base import EntityNotActive, EntityNotChange
 from src.core.exception.user import UserIsNotPremiumError
 from src.core.interfaces.unit_of_work import IUnitOfWork
 
@@ -25,17 +27,27 @@ class MissionCommunityUpdateUsecase:
         if not user.is_premium:
             raise UserIsNotPremiumError(user_id=user.id)
         async with self.uow as uow:
-            user_community = await uow.community.user_get(community_id=community_id, user_id=user.id)
-            if user_community.role in [CommunityRoleEnum.USER, CommunityRoleEnum.BLOCKED]:
-                raise PermissionError("")
             community = await uow.community.get(id=community_id)
             if not community.active:
                 raise EntityNotActive(msg="")
-            updated_mission = await uow.mission.community_mission_update(
+
+            user_community = await uow.community.user_get(community_id=community_id, user_id=user.id)
+            if user_community.role in [CommunityRoleEnum.USER, CommunityRoleEnum.BLOCKED]:
+                raise PermissionError("")
+
+            community_mission = await uow.mission.community_mission_get(id=id, community_id=community_id)
+            if community_mission.status != OccupancyStatusEnum.ACTIVE:
+                raise EntityNotChange(msg="")
+
+            updated_community_mission = await uow.mission.community_mission_update(
                 id=id, community_id=community_id, obj=update_obj
             )
             if update_obj.status == OccupancyStatusEnum.FINISH:
-                # Add score
-                ...
+                base_mission = await uow.mission.get(id=community_mission.mission_id, lang=user.language)
+                await uow.score_community.add(
+                    obj=CommunityOperationWithScoreDTO(
+                        community_id=community_id, value=base_mission.score, operation=ScoreOperationEnum.PLUS
+                    )
+                )
             await uow.commit()
-        return Result(item=updated_mission)
+        return Result(item=updated_community_mission)
--- src/core/usecases/challenges/mission/mission_user_update.py
+++ src/core/usecases/challenges/mission/mission_user_update.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 
 from src.core.dto.challenges.mission import MissionUserUpdateDTO
+from src.core.dto.user.score import OperationWithScoreUserDTO
 from src.core.entity.mission import MissionUser
 from src.core.entity.user import User
 from src.core.enum.challenges.status import OccupancyStatusEnum
+from src.core.enum.score.operation import ScoreOperationEnum
 from src.core.exception.base import EntityNotChange
 from src.core.exception.user import UserIsNotPremiumError
 from src.core.interfaces.unit_of_work import IUnitOfWork
@@ -22,12 +24,16 @@ class MissionUserUpdateUsecase:
         if not user.is_premium:
             raise UserIsNotPremiumError(user_id=user.id)
         async with self.uow as uow:
-            mission = await uow.mission.user_mission_get(id=id, user_id=user.id)
-            if mission.status != OccupancyStatusEnum.ACTIVE:
+            user_mission = await uow.mission.user_mission_get(id=id, user_id=user.id)
+            if user_mission.status != OccupancyStatusEnum.ACTIVE:
                 raise EntityNotChange(msg="")
-            updated_mission = await uow.mission.user_mission_update(id=id, user_id=user.id, obj=update_obj)
+            updated_user_mission = await uow.mission.user_mission_update(id=id, user_id=user.id, obj=update_obj)
             if update_obj.status == OccupancyStatusEnum.FINISH:
-                # Add score
-                ...
+                base_mission = await uow.mission.get(id=user_mission.mission_id, lang=user.language)
+                await uow.score_user.add(
+                    obj=OperationWithScoreUserDTO(
+                        user_id=user.id, value=base_mission.score, operation=ScoreOperationEnum.PLUS
+                    )
+                )
             await uow.commit()
-        return Result(item=updated_mission)
+        return Result(item=updated_user_mission)

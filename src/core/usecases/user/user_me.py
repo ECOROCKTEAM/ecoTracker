605cbcfb6550c7732590c96da6bef0d7ee29808a
JaysesS <myfantasyisnotworking@vk.com>
2024-01-05 21:57:41 +0300

    Core: Add UserMeUsecaseDevelop

--- src/core/usecases/user/user_me.py
+++ src/core/usecases/user/user_me.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
+from typing import Any
 
+from src.core.entity.subscription import Subscription
 from src.core.entity.user import User
+from src.core.enum.language import LanguageEnum
 from src.core.exception.base import EntityNotFound
 from src.core.exception.user import UserIsNotActivateError
 from src.core.interfaces.unit_of_work import IUnitOfWork
@@ -25,3 +28,20 @@ class UserMeUsecase:
                 raise UserIsNotActivateError(user_id=user_id)
 
             return Result(item=user)
+
+
+class UserMeUsecaseDevelop:
+    def __init__(self, uow: IUnitOfWork) -> None:
+        self.uow = uow
+
+    async def __call__(self, *, user_id: Any) -> Result:
+        return Result(
+            item=User(
+                id="id_1337_fake",
+                username="Shrek Shrekovi4 (dev)",
+                password="was removed",
+                active=True,
+                subscription=Subscription(),
+                language=LanguageEnum.EN,
+            )
+        )

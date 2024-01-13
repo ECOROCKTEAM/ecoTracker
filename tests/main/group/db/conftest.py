c5cac9652cd4a4c9e164b37124a9c6b648cbc960
JaysesS <myfantasyisnotworking@vk.com>
2024-01-13 23:31:56 +0300

    Tests: Delete old tests for group repository

--- tests/main/group/db/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.data.repository.group import IRepositoryGroup, RepositoryGroup
-
-
-@pytest.fixture(scope="function")
-def repo(session: AsyncSession) -> IRepositoryGroup:
-    return RepositoryGroup(session)

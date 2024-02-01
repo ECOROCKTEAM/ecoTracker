6236289394e0cab5e5ab306eec4843ad16db2e01
JaysesS <myfantasyisnotworking@vk.com>
2024-02-02 00:14:14 +0300

    Fix: User interface, repository and uc

--- src/core/usecases/user/user_create.py
+++ src/core/usecases/user/user_create.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 
 # from src.core.dto.mock import MockObj
-from src.core.entity.subscription import Subscription
+# from src.core.entity.subscription import Subscription
 from src.core.entity.user import User, UserCreateDTO
 from src.core.interfaces.unit_of_work import IUnitOfWork
 
@@ -18,7 +18,7 @@ class UserCreateUseCase:
     async def __call__(self, *, obj: UserCreateDTO) -> Result:
         # subscription = await self.uow.subscription.lst(filter_obj=MockObj())
         async with self.uow as uow:
-            subscription = Subscription()
-            user = await self.uow.user.create(user_obj=obj, sub_obj=subscription)
+            # subscription = Subscription()
+            user = await self.uow.user.create(obj=obj)
             await uow.commit()
         return Result(item=user)

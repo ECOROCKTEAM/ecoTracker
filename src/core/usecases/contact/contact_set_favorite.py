cc73a7bf4dad1c2cde6c51eb00456c59942a79f8
Fiz0rg <roma77093@gmail.com>
2024-02-10 11:54:49 +0100

    pre-tests commit

--- src/core/usecases/contact/contact_set_favorite.py
+++ src/core/usecases/contact/contact_set_favorite.py
@@ -31,7 +31,9 @@ class ContactUserSetFaforiteUsecase:
             if not user_contact.active:
                 raise ContactIsNotActiveError(msg=f"{id=}")
 
-            _ = await uow.user_contact.set_favorite(id=id, is_favorite=False)
+            _ = await uow.user_contact.set_favorite(id=current_favorite_user_contact.id, is_favorite=False)
+
+            await uow.commit()
 
             new_favorite_user_contact = await uow.user_contact.set_favorite(id=id, is_favorite=True)
 

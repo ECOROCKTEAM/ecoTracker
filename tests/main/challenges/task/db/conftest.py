78d0e35d547252a370b64118fb13a177d806cb5a
JaysesS <myfantasyisnotworking@vk.com>
2024-03-18 22:24:59 +0300

    Test: Remove, moved tests

--- tests/main/challenges/task/db/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.data.repository.challenges.task import IRepositoryTask, RepositoryTask
-
-
-@pytest.fixture(scope="function")
-def repo(session: AsyncSession) -> IRepositoryTask:
-    return RepositoryTask(session)

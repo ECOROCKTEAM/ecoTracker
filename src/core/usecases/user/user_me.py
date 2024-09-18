f5f49d8af5d364b3d000ea454acee7418d5fcade
Fiz0rg <roma77093@gmail.com>
2024-09-18 18:10:35 +0200

    Create user default contact when user create an account

--- src/core/usecases/user/user_me.py
+++ src/core/usecases/user/user_me.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 
+from src.core.dto.auth.firebase import UserIdentity
+from src.core.dto.m2m.user.contact import ContactUserCreateDTO
 from src.core.entity.user import User, UserCreateDTO
 from src.core.enum.language import LanguageEnum
+from src.core.enum.user.contact import ContactTypeEnum
 from src.core.exception.base import AuthError, EntityNotFound
 from src.core.exception.user import UserNotActive
 from src.core.interfaces.repository.auth import IAuthProviderRepository
@@ -35,9 +38,32 @@ class UserMeUsecase:
                         language=LanguageEnum.EN,
                     ),
                 )
+                await self.create_default_user_profile(uow=uow, user=user_identity)
                 await uow.commit()
 
         if not user.active:
             raise UserNotActive(id=user_identity.id)
 
         return Result(item=user)
+
+    async def create_default_user_profile(self, *, uow: IUnitOfWork, user: UserIdentity) -> None:
+        await self.create_user_contact(uow=uow, user=user)
+        # await self.create_user_subscription(uow=uow, user=user)
+        # await self.create_user_tasks(uow=uow, user=user)
+
+    async def create_user_contact(self, *, uow: IUnitOfWork, user: UserIdentity) -> None:
+        await uow.user_contact.create(
+            user_id=user.id,
+            obj=ContactUserCreateDTO(
+                value=user.email,
+                type=ContactTypeEnum.MAIL,
+                active=True,
+                is_favorite=True,
+            ),
+        )
+
+    # async def create_user_subscription(self, *, uow: IUnitOfWork, user: UserIdentity) -> None:
+    #     ...
+
+    # async def create_user_tasks(self, *, uow: IUnitOfWork, user: UserIdentity) -> None:
+    #     ...
